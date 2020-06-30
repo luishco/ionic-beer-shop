62d3f250e53fec6a4a5e17bc55e1bc400a92e308
luishco <luiscarvoliveira@gmail.com>
2020-06-30 10:48:22 -0300

    feat: login page

---
+++ src/app/auth/login/login.page.ts
@@ -0,0 +1,47 @@
+import { AuthService } from 'src/app/services/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  loginForm: FormGroup
+  loginError: string
+
+  constructor(private auth: AuthService, fb: FormBuilder) {    
+    this.loginForm = fb.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+    });
+  }
+
+  login () {
+    let data = this.loginForm.value;
+
+    if (!data.email) {
+      return;
+    }
+
+    let credentials = {
+      email: data.email,
+      password: data.password
+    };
+
+    this.auth.signInWithEmail(credentials).then(
+      () => console.log('logado'),
+      error => {
+        this.loginError = error.message
+        console.log(error.message)
+      }
+    );
+
+  }
+
+  ngOnInit() {
+  }
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,15 @@ export const environment = {
   production: false
 };
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyCqhsXc5wMBparPjZdFJuvk7z2D-SsBZoI",
+  authDomain: "beer-shop-55804.firebaseapp.com",
+  databaseURL: "https://beer-shop-55804.firebaseio.com",
+  projectId: "beer-shop-55804",
+  storageBucket: "beer-shop-55804.appspot.com",
+  messagingSenderId: "35973669077",
+  appId: "1:35973669077:web:803f309ed7aad0dc71425c"
+};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
