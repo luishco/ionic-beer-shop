15c2a61b86998c931bb3e449dff65baf348c35ab
luishco <luiscarvoliveira@gmail.com>
2020-06-30 14:07:42 -0300

    feat: login page css and router to register page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     path: '',
     redirectTo: 'login',
     pathMatch: 'full'
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./auth/register/register.module').then( m => m.RegisterPageModule)
   }
 ];
 
